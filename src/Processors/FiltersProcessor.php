bddc9f534b30282e8289e723e97f0b46d19af114
Daniel Castillo <danielcastillo.developer@gmail.com>
2026-03-25 15:02:01 -0400

    feat: Adecuación del procesador de filtros para reutilizarlo en los having

--- src/Processors/FiltersProcessor.php
+++ src/Processors/FiltersProcessor.php
@@ -25,22 +25,96 @@ class FiltersProcessor {
      * 
      * @var array
      */
-    protected static array $relationalOperatorsAllowed = ['=','!=','>','>=','<','<=','LIKE'];
+    private static array $relationalOperatorsAllowed = ['=','!=','>','>=','<','<=','LIKE'];
 
     /**
 	 * Lista de operadores logicos permitidos.
 	 *
 	 * @var array
 	 */
-    protected static array $logicalOperatorsAllowed = ['AND','OR'];
+    private static array $logicalOperatorsAllowed = ['AND','OR'];
 
     /**
      * Lista de formato like permitidos
      * 
      * @var array
      */
-    protected static array $likeFormatsAllowed = ['BOTH','BEFORE','AFTER'];
-    
+    private static array $likeFormatsAllowed = ['BOTH','BEFORE','AFTER'];
+
+    /**
+     * Listado de callbacks validos
+     * 
+     * @var array
+     */
+    private static array $validCallbacks = [
+        'config_key_override',
+        'field_override'
+    ];
+
+    /**
+     * Callbacks para determinados partes del proceso
+     * 
+     * @var array
+     */
+    private static array $callbacks = [];
+
+
+    /**
+     * Agregar un callback a la ejecución
+     * 
+     * @param string $key - Key del callback
+     * @param callable $callback - Callback a ejecutar
+     * 
+     * @throws FiltersProcessorException
+     * 
+     * @return bool
+     */
+    public static function addCallback(string $key, callable $callback) : bool {
+        if (!in_array($key, self::$validCallbacks)) {
+            throw new FiltersProcessorException("The callback '{$key}' is not valid.");
+        }
+
+        self::$callbacks[$key] = $callback;
+
+        return true;
+    }
+
+    /**
+     * Obtener un callback
+     * 
+     * @param string $key - Key del callback
+     * 
+     * @return callable|null
+     */
+    public static function getCallback(string $key) : callable|null {
+        return self::$callbacks[$key] ?? null;
+    }
+
+    /**
+     * Remover un callback
+     * 
+     * @param string $key - Key del callback
+     * 
+     * @return callable|null
+     */
+    public static function removeCallback(string $key) : bool {
+        if (!in_array($key, self::$validCallbacks)) {
+            return false;
+        }
+
+        unset(self::$callbacks[$key]);
+
+        return true;
+    }
+
+    public static function setLimit(int $limit) {
+        self::$limit = $limit;
+    }
+
+    public static function setIterationLimit(int $iterationLimit) {
+        self::$iterationLimit = $iterationLimit;
+    }
+
     /**
      * Procesar filtros de la consulta
      * 
@@ -197,8 +271,16 @@ class FiltersProcessor {
             throw new FiltersProcessorException("The filter configuration '{$breadcrumb}' does not have a valid field defined (index: 0).");
         }
 
-        $config = $schema ->getFieldConfig($fieldKey);
-        
+
+        $configKey = $fieldKey;
+        if (array_key_exists('config_key_override', self::$callbacks)) {
+            $configKey = self::getCallback('config_key_override')($configKey, $filter);
+            if (!is_string($configKey)) {
+                throw new FiltersProcessorException("The callback 'config_key_override' must return a string.");
+            }
+        }
+
+        $config = $schema ->getFieldConfig($configKey);        
         if (empty($config)) {
             throw new FiltersProcessorException("The field '{$fieldKey}' does not exist in the schema (breadcrumb: {$breadcrumb}; index: 0).");
         }
@@ -209,6 +291,14 @@ class FiltersProcessor {
                 throw new FiltersProcessorException("No access to the field '{$fieldKey}' (breadcrumb: {$breadcrumb}; index: 0).");
             }
         }
+
+        $field = $config['sql'];
+        if (array_key_exists('field_override', self::$callbacks)) {
+            $field = self::getCallback('field_override')($fieldKey, $config, $filter);
+            if (!is_string($field)) {
+                throw new FiltersProcessorException("The callback 'field_override' must return a string.");
+            }
+        }        
         
         // Valor
         $value = $filter[1] ?? $filter['value'] ?? null;
@@ -268,7 +358,7 @@ class FiltersProcessor {
         return [
             'field_key' => $fieldKey,
             'table_key' => $config['table'],
-            'field' => $config['sql'],
+            'field' => $field,
             'value' => $value,
             'relational_operator' => $relationalOperator,
             'logical_operator' => $logicalOperator,
